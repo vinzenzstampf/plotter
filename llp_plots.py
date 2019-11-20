f548fa0e1d5734b59f07730d1e8b98b2c8a3df11
cesare borgia <v.stampf@icloud.com>
2019-11-20 18:10:28 +0100

    saving llp plots

--- llp_plots.py
+++ llp_plots.py
@@ -1,9 +1,36 @@
 import ROOT as rt
+from rootpy.plotting import Canvas, Pad
 from ROOT import RDataFrame as rdf
-from selections import Selections as sel
+from plotter.selections import Selections as sel
+from plotter.cmsstyle import CMS_lumi
 import numpy as np
 
+def show_logo_in_prog():
+    logo = rt.TLatex()
+    logo.SetNDC()
+    logo.SetTextAlign(11)
+    logo.SetTextFont(61)
+    logo.SetTextSize(0.041)
+    logo.DrawLatex(0.15,0.88,'CMS')
+    
+    preliminary = rt.TLatex()
+    preliminary.SetNDC()
+    preliminary.SetTextAlign(11)
+    preliminary.SetTextFont(52)
+    preliminary.SetTextSize(0.035)
+    preliminary.DrawLatex(0.25,0.88,'In Progress')
+
+def show_lumi(title):
+    latex = rt.TLatex()
+    latex.SetNDC()
+    latex.SetTextAlign(31)
+    latex.SetTextFont(42)
+    latex.SetTextSize(0.031)
+    latex.DrawLatex(0.83,0.88,title)
+
 rt.ROOT.EnableImplicitMT()
+
+rt.gStyle.SetOptStat(0)
 
 cuts = sel('mmm')
 
@@ -31,11 +58,11 @@ b_sv_cos = np.linspace(0.5, 1.2, 21)
 # cut = ' & '.join( [ cuts.selections['SR_sb_no_dxy'], cuts.selections['pt_iso'], ## v1: add  vetoes 
                     # cuts.selections['vetoes_12_OS'], cuts.selections['vetoes_01_OS'], cuts.selections['vetoes_02_OS'] ] )
 
-# cut = ' & '.join( [ cuts.selections['SR_sb_w_dxy'], cuts.selections['pt_iso'], ## v2: add  dxy
-                    # cuts.selections['vetoes_12_OS'], cuts.selections['vetoes_01_OS'], cuts.selections['vetoes_02_OS'] ] ) 
+cut = ' & '.join( [ cuts.selections['SR_sb_w_dxy'], cuts.selections['pt_iso'], ## v2: add  dxy
+                    cuts.selections['vetoes_12_OS'], cuts.selections['vetoes_01_OS'], cuts.selections['vetoes_02_OS'] ] ) 
 
-cut = ' & '.join( [ cuts.selections['baseline'], cuts.selections['pt_iso'], ## v3: SR, as before
-                    cuts.selections['vetoes_12_OS'], cuts.selections['vetoes_01_OS'], cuts.selections['vetoes_02_OS'] ] )
+# cut = ' & '.join( [ cuts.selections['baseline'], cuts.selections['pt_iso'], ## v3: SR, as before
+                    # cuts.selections['vetoes_12_OS'], cuts.selections['vetoes_01_OS'], cuts.selections['vetoes_02_OS'] ] )
 
 df_0 = df.Filter(cut)
 
@@ -49,8 +76,8 @@ df_t   = df_0.Filter(cuts.selections['tight'])
 dxy  = True
 disp = True
 dr   = True
-m12  = True
 sv   = True
+m12  = True
 
 # histos
 if dxy:
@@ -69,11 +96,12 @@ if dxy:
     h_1dxy_t  .Add(h_2dxy_t)
     h_1dxy_lnt.Add(h_2dxy_lnt)
 
-    h_1dxy_lnt.SetTitle('dxy_lnt; d_{xy} (cm); %')
+    h_1dxy_lnt.SetTitle('; d_{xy} (cm); %')
+    h_1dxy_lnt.SetName('loose-not-tight')
     h_1dxy_lnt.SetLineColor(rt.kCyan+1)
     h_1dxy_lnt.SetLineWidth(2)
 
-    h_1dxy_t  .SetTitle('dxy_tight')
+    h_1dxy_t  .SetTitle('tight')
     h_1dxy_t  .SetLineColor(rt.kBlue+2)
     h_1dxy_t  .SetLineWidth(2)
     
@@ -87,11 +115,12 @@ if disp:
 
     h_disp_lnt = ph_disp_lnt.GetPtr()
 
-    h_disp_lnt.SetTitle('2d_disp_lnt; 2D displacement (cm); %')
+    h_disp_lnt.SetTitle('; L_{xy} (cm); %')
+    h_disp_lnt.SetName('loose-not-tight')
     h_disp_lnt.SetLineColor(rt.kCyan+1)
     h_disp_lnt.SetLineWidth(2)
 
-    h_disp_t  .SetTitle('2d_disp_tight')
+    h_disp_t  .SetTitle('tight')
     h_disp_t  .SetLineColor(rt.kBlue+2)
     h_disp_t  .SetLineWidth(2)
 
@@ -105,11 +134,12 @@ if dr:
 
     h_dr12_lnt = ph_dr12_lnt.GetPtr()
 
-    h_dr12_lnt.SetTitle('dr12_lnt; #Delta R; %')
+    h_dr12_lnt.SetTitle('; #Delta R_{23}; %')
+    h_dr12_lnt.SetName('loose-not-tight')
     h_dr12_lnt.SetLineColor(rt.kCyan+1)
     h_dr12_lnt.SetLineWidth(2)
 
-    h_dr12_t  .SetTitle('dr12_tight')
+    h_dr12_t  .SetTitle('tight')
     h_dr12_t  .SetLineColor(rt.kBlue+2)
     h_dr12_t  .SetLineWidth(2)
 
@@ -122,11 +152,12 @@ if sv:
 
     h_sv_cos_lnt = ph_sv_cos_lnt.GetPtr()
 
-    h_sv_cos_lnt.SetTitle('sv_cos_lnt; sv_cos; %')
+    h_sv_cos_lnt.SetTitle('; sv_cos; %')
+    h_sv_cos_lnt.SetName('loose-not-tight')
     h_sv_cos_lnt.SetLineColor(rt.kCyan+1)
     h_sv_cos_lnt.SetLineWidth(2)
 
-    h_sv_cos_t  .SetTitle('sv_cos_tight')
+    h_sv_cos_t  .SetTitle('tight')
     h_sv_cos_t  .SetLineColor(rt.kBlue+2)
     h_sv_cos_t  .SetLineWidth(2)
 
@@ -139,68 +170,110 @@ if m12:
 
     h_m12_lnt = ph_m12_lnt.GetPtr()
 
-    h_m12_lnt.SetTitle('m23_lnt; m_{23}; %')
+    h_m12_lnt.SetTitle('; m_{23} (GeV); %')
+    h_m12_lnt.SetName('loose-not-tight')
     h_m12_lnt.SetLineColor(rt.kCyan+1)
     h_m12_lnt.SetLineWidth(2)
 
-    h_m12_t  .SetTitle('m23_tight')
+    h_m12_t  .SetTitle('tight')
     h_m12_t  .SetLineColor(rt.kBlue+2)
     h_m12_t  .SetLineWidth(2)
 
 
 
 if dxy:
-    can = rt.TCanvas('llp_plot', 'llp_plot')
-    can.cd()
+    can = Canvas(width=700, height=700) ; can.Draw()
+    can.cd() ; main_pad  = Pad(0. , 0. , 1., 1.  ) ; main_pad .Draw()
+    main_pad.SetTicks(True)
+    main_pad.SetTopMargin(0.15)
+    main_pad.SetBottomMargin(0.15)
+    main_pad.SetLeftMargin(0.15)
+    main_pad.SetRightMargin(0.15)
+    main_pad.cd()
     h_1dxy_lnt.GetXaxis().SetNoExponent()
     h_1dxy_lnt.GetXaxis().SetMoreLogLabels()
     h_1dxy_lnt.DrawNormalized('histe')
     h_1dxy_t.DrawNormalized('histesame')
-    can.BuildLegend(0.65, 0.6,0.85,0.7)
-    can.SetLogx()
+    main_pad.BuildLegend(0.62, 0.6,0.82,0.7)
+    show_logo_in_prog()
+    show_lumi('2018, L = 59.7 fb^{-1}, 13 TeV')
+    main_pad.SetLogx()
     can.Modified(); can.Update()
     can.SaveAs('plots/llp/dxy.pdf')
     can.SaveAs('plots/llp/dxy.root')
 
 if disp:
-    can = rt.TCanvas('llp_plot', 'llp_plot')
-    can.cd()
+    can = Canvas(width=700, height=700) ; can.Draw()
+    can.cd() ; main_pad  = Pad(0. , 0. , 1., 1.  ) ; main_pad .Draw()
+    main_pad.SetTicks(True)
+    main_pad.SetTopMargin(0.15)
+    main_pad.SetBottomMargin(0.15)
+    main_pad.SetLeftMargin(0.15)
+    main_pad.SetRightMargin(0.15)
+    main_pad.cd()
     h_disp_lnt.GetXaxis().SetNoExponent()
     h_disp_lnt.GetXaxis().SetMoreLogLabels()
     h_disp_lnt.DrawNormalized('histe')
     h_disp_t.DrawNormalized('histesame')
-    can.BuildLegend(0.65, 0.6,0.85,0.7)
-    can.SetLogx()
+    main_pad.BuildLegend(0.62, 0.6,0.82,0.7)
+    show_logo_in_prog()
+    show_lumi('2018, L = 59.7 fb^{-1}, 13 TeV')
+    main_pad.SetLogx()
     can.Modified(); can.Update()
     can.SaveAs('plots/llp/disp.pdf')
     can.SaveAs('plots/llp/disp.root')
 
 if dr:
-    can = rt.TCanvas('llp_plot', 'llp_plot')
-    can.cd()
+    can = Canvas(width=700, height=700) ; can.Draw()
+    can.cd() ; main_pad  = Pad(0. , 0. , 1., 1.  ) ; main_pad .Draw()
+    main_pad.SetTicks(True)
+    main_pad.SetTopMargin(0.15)
+    main_pad.SetBottomMargin(0.15)
+    main_pad.SetLeftMargin(0.15)
+    main_pad.SetRightMargin(0.15)
+    main_pad.cd()
     h_dr12_lnt.DrawNormalized('histe')
     h_dr12_t.DrawNormalized('histesame')
-    can.BuildLegend(0.65, 0.6,0.85,0.7)
+    main_pad.BuildLegend(0.62, 0.6,0.82,0.7)
+    show_logo_in_prog()
+    show_lumi('2018, L = 59.7 fb^{-1}, 13 TeV')
     can.Modified(); can.Update()
     can.SaveAs('plots/llp/dr12.pdf')
     can.SaveAs('plots/llp/dr12.root')
     
 if sv:
-    can = rt.TCanvas('llp_plot', 'llp_plot')
-    can.cd()
+    can = Canvas(width=700, height=700) ; can.Draw()
+    can.cd() ; main_pad  = Pad(0. , 0. , 1., 1.  ) ; main_pad .Draw()
+    main_pad.SetTicks(True)
+    main_pad.SetTopMargin(0.15)
+    main_pad.SetBottomMargin(0.15)
+    main_pad.SetLeftMargin(0.15)
+    main_pad.SetRightMargin(0.15)
+    main_pad.cd()
     h_sv_cos_lnt.DrawNormalized('histe')
     h_sv_cos_t.DrawNormalized('histesame')
-    can.BuildLegend(0.65, 0.6,0.85,0.7)
+    main_pad.BuildLegend(0.62, 0.6,0.82,0.7)
+    show_logo_in_prog()
+    show_lumi('2018, L = 59.7 fb^{-1}, 13 TeV')
     can.Modified(); can.Update()
     can.SaveAs('plots/llp/sv_cos.pdf')
     can.SaveAs('plots/llp/sv_cos.root')
     
 if m12:
-    can = rt.TCanvas('llp_plot', 'llp_plot')
-    can.cd()
+    can = Canvas(width=700, height=700) ; can.Draw()
+    can.cd() ; main_pad  = Pad(0. , 0. , 1., 1.  ) ; main_pad .Draw()
+    main_pad.SetTicks(True)
+    main_pad.SetTopMargin(0.15)
+    main_pad.SetBottomMargin(0.15)
+    main_pad.SetLeftMargin(0.15)
+    main_pad.SetRightMargin(0.15)
+    main_pad.cd()
     h_m12_lnt.DrawNormalized('histe')
     h_m12_t.DrawNormalized('histesame')
-    can.BuildLegend(0.65, 0.6,0.85,0.7)
+    main_pad.BuildLegend(0.62, 0.6,0.82,0.7)
+    show_logo_in_prog()
+    show_lumi('2018, L = 59.7 fb^{-1}, 13 TeV')
     can.Modified(); can.Update()
     can.SaveAs('plots/llp/m12.pdf')
     can.SaveAs('plots/llp/m12.root')
+
