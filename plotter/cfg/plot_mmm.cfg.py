08d42985c1d0e014934bfe588d54738c12f3e24d
cesare borgia <v.stampf@icloud.com>
2020-01-16 11:57:26 +0100

    merging complete

--- plotter/cfg/plot_mmm.cfg.py
+++ plotter/cfg/plot_mmm.cfg.py
@@ -57,20 +57,20 @@ plotter = Plotter (channel          = ch,
                    datacards        = ['hnl_m_12_lxy_lt_0p5', 'hnl_m_12_lxy_0p5_to_1p5', 'hnl_m_12_lxy_1p5_to_4p0', 'hnl_m_12_lxy_mt_4p0'], # FIXME! improve this to accept wildcards / regex
                    )
 
-plotter_no_dxy = Plotter (channel         = ch,
-                   base_dir        = env['NTUPLE_DIR'],
-                   post_fix        = 'HNLTreeProducer/tree.root', # 'HNLTreeProducer_%s/tree.root' %ch,
-                   selection_data  = selection,
-                   selection_mc    = selection + [cuts.selections['is_prompt_lepton']],
-                   selection_tight = cuts.selections_pd['tight'],
-                   lumi            = 59700.,
-                   model           = env['NN_DIR'] + '/mmm_191121_16h_25m/net_model_weighted.h5', 
-                   transformation  = env['NN_DIR'] + '/mmm_191121_16h_25m/input_tranformation_weighted.pck',
-                   features        = env['NN_DIR'] + '/mmm_191121_16h_25m/input_features.pck',
-                   process_signals = False,
-                   plot_signals    = False,
-                   blinded         = False,
-                   )
+# plotter_no_dxy = Plotter (channel         = ch,
+                   # base_dir        = env['NTUPLE_DIR'],
+                   # post_fix        = 'HNLTreeProducer/tree.root', # 'HNLTreeProducer_%s/tree.root' %ch,
+                   # selection_data  = selection,
+                   # selection_mc    = selection + [cuts.selections['is_prompt_lepton']],
+                   # selection_tight = cuts.selections_pd['tight'],
+                   # lumi            = 59700.,
+                   # model           = env['NN_DIR'] + '/mmm_191121_16h_25m/net_model_weighted.h5', 
+                   # transformation  = env['NN_DIR'] + '/mmm_191121_16h_25m/input_tranformation_weighted.pck',
+                   # features        = env['NN_DIR'] + '/mmm_191121_16h_25m/input_features.pck',
+                   # process_signals = False,
+                   # plot_signals    = False,
+                   # blinded         = False,
+                   # )
 
 if __name__ == '__main__':
     plotter.plot()
