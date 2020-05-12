5c6e94ecdd7ff2b6a41749b028f64572e40d4333
cesare borgia <v.stampf@icloud.com>
2020-05-12 16:41:00 +0200

    adding path dummies for AM & MG

--- plotter/utils.py
+++ plotter/utils.py
@@ -6,19 +6,30 @@ from getpass import getuser as user
 import pickle
 
 def set_paths(channel, year):
+   
+    env['NTUPLE_DIR']      = '<DUMMY>'
+    
     assert channel in ['mmm', 'mem', 'mem_os', 'mem_ss', 'eem', 'eem_os', 'eem_ss', 'eee'], 'ERROR: Channel not valid.'
     assert year in [2016, 2017, 2018], 'ERROR: Year not valid.'    
     if user() == 'manzoni': 
         env['NTUPLE_BASE_DIR'] = '/Users/manzoni/Documents/HNL/ntuples/'
-        env['NTUPLE_DIR']      = '/Users/manzoni/Documents/HNL/ntuples/%d/%s' %(year, channel.split('_')[0])
         env['PLOT_DIR']        = '/Users/manzoni/Documents/HNL/plotter/plots_%d_%s/' %(year, channel)
         env['NN_DIR']          = '/Users/manzoni/Documents/HNL/NN/'
 
     if user() == 'cesareborgia': 
         env['NTUPLE_BASE_DIR'] = '/Users/cesareborgia/cernbox/hnl/'
-        env['NTUPLE_DIR']      = '/Users/cesareborgia/cernbox/ntuples/%d/%s' %(year, channel.split('_')[0])
         env['PLOT_DIR']        = '/Users/cesareborgia/Dropbox/documents/physics/phd/plots/%d/%s/' %(year, channel)
         env['NN_DIR']          = '/Users/cesareborgia/HNL/plotter/NN/trainings/'
+
+    if user() == '<AM_user_name>': 
+        env['NTUPLE_BASE_DIR'] = '/Users/<AM_hnl_dir>/hnl/'
+        env['PLOT_DIR']        = '/Users/<AM_plot_dir>/%d/%s/' %(year, channel)
+        env['NN_DIR']          = '/Users/<AM_plotter_dir>/plotter/NN/trainings/'
+
+    if user() == '<MG_user_name>': 
+        env['NTUPLE_BASE_DIR'] = '/Users/<MG_hnl_dir>/hnl/'
+        env['PLOT_DIR']        = '/Users/<MG_plot_dir>/%d/%s/' %(year, channel)
+        env['NN_DIR']          = '/Users/<MG_plotter_dir>/plotter/NN/trainings/'
 
 def get_time_str():
     today   = datetime.now()
